4c86a84f4bb5dffad182c76167cfd2177ebae30a
gr1mw1k <ameyashinde3@gmail.com>
2023-08-30 18:38:23 +0530

    Sticky Notes

---
+++ script.js
@@ -0,0 +1,34 @@
+const note = document.getElementById("input");
+const addbtn = document.getElementById("add");
+const list = document.getElementById("notes-list");
+const col = document.getElementById("col");
+const empty = document.getElementById("no-notes");
+
+addbtn.addEventListener("click", () => {
+    if (note.value) {
+        empty.innerHTML = "";
+        createNote();
+    } else {
+        alert("A note can't be empty.");
+    }
+});
+
+function createNote() {
+    const div = document.createElement("div");
+    div.style.backgroundColor = col.value;
+    div.className = "notes";
+    div.innerHTML = `<p>${note.value}</p>
+                     <a class="delBtn">Del</a>`;
+    list.appendChild(div);
+    note.value = "";
+
+    const delBtn = div.querySelector(".delBtn");
+    delBtn.addEventListener("click", () => {
+        list.removeChild(div);
+    });
+}
+
+
+
+
+
